bca02d7a1d00ebd8a8a375c8992e4ecdc8c39f79
Anna <lozynska.anna@gmail.com>
2017-03-05 22:12:27 +0000

    new

--- js/index.js
+++ js/index.js
@@ -1,13 +1,30 @@
+function menu(id) {
+    var menu = document.getElementById('menu_1').style;
+    if (menu.display == 'none') {
+        menu.display = 'block';
+    }
+    else {
+        menu.display = 'none';
+    }
+}
+menu1.addEventListener('click', function() {menu ((menu_1))});
 
-function Menu(id)
-{
-  var menu = document.getElementById('menu_1').style;  
-if (menu.display == 'none')
-{
-menu.display = 'block';
-}
-else
-{
-menu.display = 'none';
-}
-}
+$(function() {
+    $('#btn-submit').click(function(e) {
+       e.preventDefault();
+       $.ajax({
+            url: "https://formspree.io/lozynska.anna@gmail.com", 
+            method: "POST",
+            data: {
+                clientName: $('#client-name').val(),
+                clientEmail: $('#client-email').val(),
+                clientPhonenumber: $('#client-phonenumber').val(),
+                comment: $('#comment').val()
+            },
+            dataType: "json"
+       })
+         .done(function() {
+             $('form').html('<h1>Thank you for contacting us!</h1>');
+         });
+    });    
+});
